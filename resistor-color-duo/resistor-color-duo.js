eadcf764a6660e90d79d260f95597affa6d8a5e6
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-09-22 20:50:42 +0100

    Improved Solution. Map instead of forEach. Number() instead of ParseInt and used join() instead of initializing an empty str variable

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,9 +1,9 @@
 export const value = (colors) => {
-  let number = "";
-  colors.forEach(color => {
-    number = number + COLORS.indexOf(color);
-  });
-  return parseInt(number);
+  colors = colors.map(color => {
+    return COLORS.indexOf(color);
+  }).join('');
+
+  return Number(colors);
 };
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
