f9953a56129561364c889cb8711cecadd166dd1b
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-10-15 22:57:21 +0100

    another solution without repeating earthYear in object properties

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,15 +1,16 @@
 const earthYear = 31557600;
 export const age = (planet, ageInSeconds) => {
   const orbitalPeriods = {
-    mercury: earthYear * 0.2408467,
-    venus: earthYear * 0.61519726,
+    mercury: 0.2408467,
+    venus: 0.61519726,
     earth: earthYear,
-    mars: earthYear * 1.8808158,
-    jupiter: earthYear * 11.862615,
-    saturn: earthYear * 29.447498,
-    uranus: earthYear * 84.016846,
-    neptune: earthYear * 164.79132,
-  }
-  const ageInPlanet = (ageInSeconds / orbitalPeriods[planet]).toFixed(2);
-  return Number(ageInPlanet);
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  };
+  const ageInPlanet = (ageInSeconds / (orbitalPeriods[planet] * earthYear)).toFixed(2);
+
+  return orbitalPeriods[planet] === earthYear ? Number((ageInSeconds / earthYear).toFixed(2)) : Number(ageInPlanet);
 };
