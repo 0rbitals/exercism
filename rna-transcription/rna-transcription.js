2e87ed4968e882e92599cfaa94c79d5747202e14
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-10-15 17:45:55 +0100

    improved rna-transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,17 +1,10 @@
 export const toRna = (dnaStrands) => {
-  const rna = dnaStrands.split('').map((strand) => {
-    switch(strand) {
-      case 'G':
-        return 'C';
-      case 'C':
-        return 'G'
-      case 'T':
-        return 'A'
-      case 'A':
-        return 'U'
-      default:
-        return ''
-    }
-  });
+  const dnaToRNA = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U'
+  };
+  const rna = dnaStrands.split('').map((strand) => dnaToRNA[strand]);
   return rna.join('');
 };
