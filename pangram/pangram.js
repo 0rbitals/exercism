f731a7b318228610d9cf0304dd4ad9fcc4c468ba
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-10-22 19:39:48 +0100

    implemented an easier to read solution for Pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,10 +1,6 @@
 export const isPangram = (text) => {
-  const arr = text.toLowerCase().split('');
-  let isPangram = true;
-  for (let i = 97; i < 123; i++) {
-    if(isPangram) {
-      isPangram = arr.includes(String.fromCharCode(i));
-    }
-  }
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+  const lettersInText = text.toLowerCase().split('');
+  const isPangram = alphabet.every((letter) => lettersInText.includes(letter));
   return isPangram;
-  };
+};
