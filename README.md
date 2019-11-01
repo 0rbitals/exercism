ea185c6c79910b7bb324067aeb97d244402177aa
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-11-02 00:56:10 +0100

    solved triangle problem

--- README.md
+++ README.md
@@ -25,3 +25,4 @@
 * Meetup
 * Clock
 * Collatz' Conjecture
+* Triangle
--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,12 +1,23 @@
 export class Triangle {
-  constructor(length1, length2, length3) {
-    
+  constructor(x, y, z) {
+    this.arr = [x, y, z].sort((a,b) => a - b);
   }
 
   kind() {
-
+    if (this.arr.reduce((acc, curr) => acc + curr) > 0) {
+      if (this.arr[2] <= this.arr[0] + this.arr[1]) {
+        if (this.arr[0] === this.arr[2]) {
+          return 'equilateral';
+        } else if (this.arr[0] !== this.arr[1] && this.arr[1] !== this.arr[2]) {
+          return 'scalene';
+        } else {
+          return 'isosceles';
+        }
+      } else {
+        throw new Error('violating triangle inequality');
+      }
+    } else {
+      throw new Error('triangle with no size');
+    }
   }
 }
-
-const test = new Triangle(4, 4, 4);
-console.log(test);
