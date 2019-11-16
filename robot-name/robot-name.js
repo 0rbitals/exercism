edd5f36832445d0a2027a847eb43a0668beedac2
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-11-17 00:47:18 +0100

    implemented some functions

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,26 +1,47 @@
-function Range(start, stop) {
-  // Get numbers from start to stop
+function randomFromRange(start, stop) {
+  let range = [...Array(stop+1).keys()];
+  range = range.slice(start);
+  return range[Math.floor(Math.random() * range.length)];
 }
 
 export class Robot {
   constructor() {
-    this.name = this.GenerateName();
+    this.name = this.GenerateName().join('');
+  }
+
+  AddToSet(name) {
+    let set = new Set();
+    set.add(name.join(''));
+    this.set = set;
   }
 
   GenerateName() {
     let name = [];
     for (let i = 0; i < 5; ++i) {
       if (i <= 1) {
-        name.push(Math.random() * 10);
+        let random = randomFromRange(65, 90);
+        name.push(String.fromCharCode(random));
       } else {
-        name.push(String.fromCharCode(Math.random()));
+        name.push(Math.floor(Math.random() * 10));
       }
     }
+    this.AddToSet(name);
     return name;
+  }
+
+  reset() {
+    let newName = this.GenerateName().join('');
+    if (this.set.has(newName)) {
+      newName = this.GenerateName().join('');
+    }
+    this.name = this.GenerateName().join('');
+    this.set.add(newName);
   }
 
  }
 
 Robot.releaseNames = () => { };
-
-console.log(new Robot())
+const robot = new Robot()
+console.log(robot.name)
+console.log(robot.reset())
+console.log(robot)
