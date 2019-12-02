686069dbf83371e1558f566e4388f83341702df6
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-12-02 23:40:19 +0100

    typo

--- acronym/acronym.js
+++ acronym/acronym.js
@@ -1,5 +1,5 @@
 export const parse = (phrase) => {
-  //Remove apostophes & underscores to make it easie
+  //Remove apostophes & underscores to make it easier
   phrase = phrase.replace("'", '').replace('_', '');
   return phrase.match(/\b[a-zA-Z]/g).join('').toUpperCase();
 };
